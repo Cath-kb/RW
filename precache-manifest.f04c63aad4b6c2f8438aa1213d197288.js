c52b29fad8b8ccf2f67f5564b87720b0c0d10e9b
Catherine Chernysheva <catherine.nikolaev@gmail.com>
2019-04-07 16:15:07 +0300

    deploy RW First APP

---
+++ precache-manifest.f04c63aad4b6c2f8438aa1213d197288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e33379da4d9fc8f16a3e",
+    "url": "./static/js/main.5a36d7ca.chunk.js"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "./static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "e33379da4d9fc8f16a3e",
+    "url": "./static/css/main.9a0efe0d.chunk.css"
+  },
+  {
+    "revision": "14409b7a997772cfd15199a9c40f4861",
+    "url": "./index.html"
+  }
+];
